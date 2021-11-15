a72d8c6e2e58d5d7a01562dea56d006b6f124391
Kikoken <kikoken@gmail.com>
2021-11-15 09:10:54 -0300

    feat: update main message@

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3001
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('Hello and Nice Day')
 })
 
 app.listen(PORT, () => console.log(`Running listen *:${PORT}`))
